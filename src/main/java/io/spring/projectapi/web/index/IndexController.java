9d5389487e7395e1b0336ed3d439c62d2c88ffe1
Madhura Bhave <bhavem@vmware.com>
2023-01-13 13:36:25 -0800

    Add index controller

---
+++ src/main/java/io/spring/projectapi/web/index/IndexController.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.projectapi.web.index;
+
+import io.spring.projectapi.web.project.ProjectsController;
+import io.spring.projectapi.web.repository.RepositoriesController;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/**
+ * Lists all resources at the root of the API.
+ *
+ * @author Madhura Bhave
+ */
+@RestController
+@RequestMapping(path = "/", produces = MediaTypes.HAL_JSON_VALUE)
+public class IndexController {
+
+	@GetMapping
+	public RepresentationModel index() {
+		return RepresentationModel.of(null).add(
+				linkTo(methodOn(ProjectsController.class).projects()).withRel("projects"),
+				linkTo(methodOn(RepositoriesController.class).repositories()).withRel("repositories"));
+	}
+
+}
---
+++ src/test/java/io/spring/projectapi/web/index/IndexControllerTests.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.projectapi.web.index;
+
+import io.spring.projectapi.test.WebApiTest;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.restdocs.hypermedia.LinkDescriptor;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Tests for {@link IndexController}
+ */
+@WebApiTest(IndexController.class)
+public class IndexControllerTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void indexReturnProjectsAndRepositories() throws Exception {
+		this.mvc.perform(get("/").accept(MediaTypes.HAL_JSON)).andExpect(status().isOk()).andDo(document("show-index",
+				preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()), links(indexLinks())));
+	}
+
+	LinkDescriptor[] indexLinks() {
+		return new LinkDescriptor[] {
+				linkWithRel("projects").optional().description("Link to <<project, Project resources>>"),
+				linkWithRel("repositories").optional().description("Link to <<repository, Repository resources>>") };
+	}
+
+}
